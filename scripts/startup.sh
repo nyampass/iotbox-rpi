78b8a4b4d53085267c857ff5ccf44749f36977aa
Tokusei Noborio <tokusei@nyampass.com>
2017-08-15 07:34:16 +0900

    Implement RPi led blink on startup

--- scripts/startup.sh
+++ scripts/startup.sh
@@ -2,10 +2,25 @@
 
 DONE_FILE=/root/.iotbox-done
 
+led_blink() {
+    echo timer > /sys/class/leds/led0/trigger
+    echo 250 > /sys/class/leds/led0/delay_on
+    echo 250 > /sys/class/leds/led0/delay_off
+}
+
+led_on() {
+ echo none > /sys/class/leds/led0/trigger
+ echo 1 > /sys/class/leds/led0/brightness
+}
+
 if [ -e $DONE_FILE ]
 then
     echo "exists $DONE_FILE"
 else
+    led_blink
+
+    cd /root
+
     nohup sed -i.bak -e "s%http://mirrordirector.raspbian.org/raspbian/%http://ftp.jaist.ac.jp/raspbian%g" /etc/apt/sources.list
 
     apt-get update
@@ -16,6 +31,8 @@ else
     npm install -g iotbox-rpi
 
     touch $DONE_FILE
+
+    led_on
 fi
 
 sudo -u pi "nohup iotbox-rpi"
